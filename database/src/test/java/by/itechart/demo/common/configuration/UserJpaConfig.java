f0f5521bf87314eca2962a83acc3a88293cb9d9a
chenzhen10 <teekeen32@gmail.com>
2019-10-25 17:49:20 +0300

    Master -delete files which was interfere with loading

--- database/src/test/java/by/itechart/demo/common/configuration/UserJpaConfig.java
+++ database/src/test/java/by/itechart/demo/common/configuration/UserJpaConfig.java
@@ -1,7 +1,12 @@
 package by.itechart.demo.common.configuration;
 
 
-
+import by.itechart.demo.post.dto.CreatePostDto;
+import by.itechart.demo.post.model.Post;
+import by.itechart.demo.user.dto.CreateUserDto;
+import by.itechart.demo.user.model.User;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +19,7 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
@@ -68,6 +74,22 @@ public class UserJpaConfig {
     }
 
 
+    @Bean
+    public ModelMapper modelMapper(){
+        ModelMapper mapper = new ModelMapper();
 
+        TypeMap<CreatePostDto, Post> createPostMapping = mapper.createTypeMap(CreatePostDto.class,Post.class);
+        createPostMapping.addMappings(m -> m.skip(Post::setId));
+
+        TypeMap<CreateUserDto, User> createUserMapping = mapper.createTypeMap(CreateUserDto.class,User.class);
+        createUserMapping.addMappings(m -> m.skip(User::setId));
+
+        return mapper;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
 
